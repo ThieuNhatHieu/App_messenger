e54c0d566d34eb4c14f2eb52ddf5eb7c23c0e6d1
Nhathieu695 <thieunhathieu511@gmail.com>
2023-09-17 21:44:46 +0700

    sign_in and sign_up

---
+++ app/src/main/java/com/example/app_messenger/Fragment/ChatFragment.kt
@@ -0,0 +1,21 @@
+package com.example.app_messenger.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.app_messenger.R
+
+class ChatFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/app_messenger/Modals/RecentChats.kt
@@ -0,0 +1,4 @@
+package com.example.app_messenger.Modals
+
+class RecentChats {
+}
